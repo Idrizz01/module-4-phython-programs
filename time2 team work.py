af08840d872717d0ff369f4482f043b7f64cc7ea
annie-82 <92351283+annie-82@users.noreply.github.com>
2021-10-30 21:50:12 -0500

    Update time2 team work.py

--- time2 team work.py
+++ time2 team work.py
@@ -1,8 +1,17 @@
 str_time = input("What time is it now?")
 str_wait_time = input("What is the number of nours to wait?")
 time = int(str_time)
+'''
+Name error
+wait_time = int(str_wait_time)
+'''
 wai_time = int(str_wait_time)
 
+'''
+Logic error :time_when_alarm_go_off is expressed as hours in (0-23)
+time_when_alarm_go_off = (time+ wait_time) % 24
+
+'''
 time_when_alarm_go_off = time + wait_time
 print(time_when_alarm_go_off)
 
