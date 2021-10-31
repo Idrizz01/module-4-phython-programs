c41b1d029955e06d1feee0bae31ffa3744d10305
annie-82 <92351283+annie-82@users.noreply.github.com>
2021-10-30 21:46:47 -0500

    Update time team work.py

--- time team work.py
+++ time team work.py
@@ -1,8 +1,23 @@
 currentTimeStr = input("What is the current time (in hours 0-23)?")
 waitTimeStr = input("How many hours do you want to wait") #a closing parenthesis was missing
 
+'''
+Name errors found
+currenTimeInt = int(currentTimeStr)
+waitTimeInt = int(waitTimeStr)
+'''
+
 currentTimeInt = int(current_time_str)
 waitTimeInt = int(wait_time_str)
 
+'''
+Logic error: The final time is represented in hours(0-23). 
+finalTimeInt = (currentTimeInt + waitTimeInt) % 24
+'''
 finalTimeInt = currentTimeInt + waitTimeInt
+
+'''
+Name error found
+print(finalTimeInt)
+'''
 print(finalTime_Int)
