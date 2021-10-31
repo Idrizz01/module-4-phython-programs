f9ea82913c14f718861cdae7fb6aad90d54e5455
annie-82 <92351283+annie-82@users.noreply.github.com>
2021-10-30 21:41:45 -0500

    Update pirate team work.py

--- pirate team work.py
+++ pirate team work.py
@@ -1,5 +1,14 @@
 greeting = input("Hello, possible pirate! What's the password?")  # a quotation mark was missing at the end
 if greeting in ["Arrr!"]:  #used the same parenthesis intead of different ones
 	print("Go away, pirate.")
+   
+
+'''
+for not matching case, the following message is printed.
+
+else:
+     print("Greetings, hater of pirates!")
+''''
+
 elif greeting in ["Not pirate"]:  #Added a new phrase in fornt of th elif with the right quotes and parenthesis
     print("Greetings, hater of pirates!") #all we added was just make the right indentation
